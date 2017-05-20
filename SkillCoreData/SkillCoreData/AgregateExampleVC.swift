0c6be3584139fb52a3b0e508a9de6137e98cf6b3
adeveloper2015 <hudiakov@gmail.com>
2017-05-20 12:50:49 +0300

    add Person

---
+++ SkillCoreData/SkillCoreData/AgregateExampleVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SkillCoreData
+//
+//  Created by adeveloper on 20.05.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+class AgregateExampleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
+
---
+++ SkillCoreData/SkillCoreData/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  SkillCoreData
+//
+//  Created by adeveloper on 20.05.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Person: NSManagedObject {
+
+}
